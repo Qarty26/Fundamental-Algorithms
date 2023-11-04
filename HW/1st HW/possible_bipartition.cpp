715a37e932fb462cf752753e4d8f17e2ddc31afa
Qarty26 <116076301+Qarty26@users.noreply.github.com>
2023-11-04 23:36:15 +0200

    much faster solution

--- HW/1st HW/possible_bipartition.cpp
+++ HW/1st HW/possible_bipartition.cpp
@@ -5,85 +5,69 @@ using namespace std;
 
 class Solution {
 public:
-    bool possibleBipartition(int n, vector<vector<int>>& dislikes) {
 
-        int culori[n+1],i,m;
-        m=n;
+
+    bool isBipartite(int n, vector<vector<int>>& connect)
+    {
         queue<int> coada;
-        for(i=1;i<=n;i++)
-            culori[i]=0;
-if(n>1){
-        coada.push(dislikes[0][0]);
+        vector<int> culori (n+1,0);
+        int m=n;
+
+        coada.push(connect[1][0]);
         culori[coada.front()]=1;
 
-while(m>0)
-{
-    //cout<<"New while, front is"<<coada.front()<<endl;
+        while(m>0)
+        {
+            int head = coada.front();
+            for(int i=0;i<connect[head].size();i++)
+            {
+                if(culori[connect[head][i]] == 0)
+                {
+                    coada.push(connect[head][i]);
+                    culori[connect[head][i]] = 3 - culori[head];
+                }
+                else if(culori[head] == culori[connect[head][i]])
+                    return false;
 
+            }
+            coada.pop();
+            m--;
+
+            if(coada.empty() && m!=0)
+                for(int j=1;j<=n;j++)
+                    if(culori[j]==0)
+                    {
+                       coada.push(j);
+                       break;
+                    }
+
+        }
+        return true;
+    }
+
+
+
+    bool possibleBipartition(int n, vector<vector<int>>& dislikes)
+    {
+
+        int i;
+        vector<vector<int>> connect;
+
+        for(i=0;i<=n;i++)
+            connect.push_back({});
         for(i=0;i<dislikes.size();i++)
         {
-            if(coada.front() == dislikes[i][0])
-           {
-                if(culori[dislikes[i][1]]==0)
-                {
-                    coada.push(dislikes[i][1]);
-                    //cout<<"Added "<<dislikes[i][1]<<endl;
-                    if(culori[coada.front()]==1) culori[dislikes[i][1]]=2;
-                        else culori[dislikes[i][1]]=1;
-                }
-
-                else if(culori[dislikes[i][1]]==culori[coada.front()])
-                {
-                    //cout<<"Am crapat, cumva..";
-                    return false;
-                }
-
-           }
-
-
-            if(coada.front() == dislikes[i][1])
-           {
-                if(culori[dislikes[i][0]]==0)
-                {
-                    coada.push(dislikes[i][0]);
-                    //cout<<"Added "<<dislikes[i][0]<<endl;
-                    if(culori[coada.front()]==1) culori[dislikes[i][0]]=2;
-                        else culori[dislikes[i][0]]=1;
-                }
-
-                else if(culori[dislikes[i][0]]==culori[coada.front()])
-                {
-                    //cout<<"Am crapat, cumva..";
-                    return false;
-                }
-           }
+            connect[dislikes[i][0]].push_back(dislikes[i][1]);
+            connect[dislikes[i][1]].push_back(dislikes[i][0]);
         }
-        //cout<<"Removed "<<coada.front()<<endl;
-        coada.pop();
-        m--;
-        //cout<<"m="<<m<<endl;
-
-        if(coada.empty() && m!=0)
-            for(i=1;i<=n;i++)
-                if(culori[i]==0)
-                {
-                   coada.push(i);
-                   break;
-                }
-
-}
-
-       /* while(!coada.empty())
-        {
-            cout<<"("<<coada.front()<<","<<culori[coada.front()]<<") ";
-            //cout<<coada.front()<<" ";
-            coada.pop();
-        }*/
 
 
 
-    }
-    return true;
+
+        if(n>1)
+            return isBipartite(n,connect);
+
+        return true;
     }
 
 };
@@ -91,8 +75,8 @@ while(m>0)
 int main()
 {
     Solution s;
-    std::vector<std::vector<int>> dislikes = {};
-    cout<<s.possibleBipartition(1,dislikes);
+    std::vector<std::vector<int>> dislikes = {{1,2},{1,3},{2,4}};
+    cout<<s.possibleBipartition(4,dislikes);
     return 0;
 }
 
