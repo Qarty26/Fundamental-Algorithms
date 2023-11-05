9067fa400fdddd0c68cf27c22f07b0bb4a1754bb
Qarty26 <116076301+Qarty26@users.noreply.github.com>
2023-11-05 19:34:26 +0200

    Add files via upload

--- HW/1st HW/graph.cpp
+++ HW/1st HW/graph.cpp
@@ -1,20 +1,14 @@
 #include <iostream>
 #include <vector>
 #include <queue>
+#include <cstring>
 
 using namespace std;
 
 class Graph
 {
-    vector<vector<int>> connect;
-    vector<int> nivel;
-    vector<int> nivel_min;
-    vector<int> viz;
-    vector<int> safe;
-    queue<vector<int>> coada;
 
-
-    void st_land(int &x, int &y) //finding the first patch of island, helping function for shortest bridge
+    void st_land(int &x, int &y,vector<vector<int>>& grid_copy) //finding the first patch of island, helping function for shortest bridge
     {
         for(int i=0;i<grid_copy.size();i++)
             for(int j=0;j<grid_copy[i].size();j++)
@@ -26,34 +20,35 @@ class Graph
                 }
     }
 
-    void filling(int x, int y) //marking the first island, helping function for shortest bridge
+    void filling(int x, int y,vector<vector<int>>& grid_copy) //marking the first island, helping function for shortest bridge
     {
         grid_copy[x][y]=2;
 
         if(0<=x-1 && x-1<grid_copy.size())
         {
             if(grid_copy[x-1][y]==1)
-                filling(x-1,y);
+                filling(x-1,y,grid_copy);
         }
         if(0<=x+1 && x+1<grid_copy.size())
         {
             if(grid_copy[x+1][y]==1)
-                filling(x+1,y);
+                filling(x+1,y,grid_copy);
         }
         if(0<=y-1 && y-1<grid_copy.size())
         {
             if(grid_copy[x][y-1]==1)
-                filling(x,y-1);
+                filling(x,y-1,grid_copy);
         }
         if(0<=y+1 && y+1<grid_copy.size())
         {
             if(grid_copy[x][y+1]==1)
-                filling(x,y+1);
+                filling(x,y+1,grid_copy);
         }
     }
 
-    void bordering() // making a border around the map, helping function for shortest bridge
+    queue<vector<int>> bordering(vector<vector<int>>& grid_copy) // making a border around the island
     {
+        queue<vector<int>> coada;
         for(int x=0;x<grid_copy.size();x++)
         {
             for(int y=0;y<grid_copy[x].size();y++)
@@ -96,57 +91,11 @@ class Graph
                     }
                 }
         }
+        return coada;
     }
 
-
-public:
-
-    void Dfs(int node) //helping method for critical connections
+    int searching(queue<vector<int>> coada,vector<vector<int>> grid_copy)
     {
-        viz[node] = 1;
-        nivel_min[node] = nivel[node];
-        for(int i=0;i<connect[node].size();i++)
-        {
-            int vecin=connect[node][i];
-            if(viz[vecin]==0)
-            {
-                nivel[vecin] = nivel[node]+1;
-                Dfs(vecin);
-                nivel_min[node] = nivel_min[node] < nivel_min[vecin] ? nivel_min[node] : nivel_min[vecin];
-                if(nivel_min[vecin]>nivel[node]) rez.push_back({vecin,node});
-            }
-            else
-                if(nivel[vecin] < nivel[node]-1)
-                    nivel_min[node] = nivel_min[node] < nivel[vecin] ? nivel_min[node] : nivel[vecin];
-
-        }
-    }
-
-    int dfs(int node) //returns 0 if the node is not safe, 1 if it is safe
-    {
-        if(safe[node]!=-1)
-            return safe[node];
-
-        safe[node] = 0;
-        for(int j=0;j<connect[node].size();j++)
-            if(dfs(connect[node][j])==0)
-                return safe[node];
-
-        safe[node] = 1;
-        return safe[node];
-    }
-
-    int BfsSearch(vector<vector<int>> grid) //searching by layers to find the 2nd island, helping function for shortest bridge
-    {
-
-        int x,y,n;
-        vector<vector<int>> grid_copy;
-        grid_copy=grid;
-        n=grid_copy.size();
-        st_land(x,y);
-        filling(x,y);
-        bordering();
-
         while(!coada.empty())
         {
             if(0<=coada.front()[0]-1 && coada.front()[0]-1<grid_copy.size() && grid_copy[coada.front()[0]-1][coada.front()[1]]==0)
@@ -186,8 +135,170 @@ public:
             coada.pop();
 
         }
+    }
+
+    void Dfs(int node,vector<vector<int>>& connect,vector<int> &nivel,vector<int> &nivel_min,vector<int> &viz,vector<vector<int>> &rez) //helping method for critical connections
+    {
+        viz[node] = 1;
+        nivel_min[node] = nivel[node];
+        for(int i=0;i<connect[node].size();i++)
+        {
+            int vecin=connect[node][i];
+            if(viz[vecin]==0)
+            {
+                nivel[vecin] = nivel[node]+1;
+                Dfs(vecin,connect,nivel,nivel_min,viz,rez);
+                nivel_min[node] = nivel_min[node] < nivel_min[vecin] ? nivel_min[node] : nivel_min[vecin];
+                if(nivel_min[vecin]>nivel[node]) rez.push_back({vecin,node});
+            }
+            else
+                if(nivel[vecin] < nivel[node]-1)
+                    nivel_min[node] = nivel_min[node] < nivel[vecin] ? nivel_min[node] : nivel[vecin];
+
+        }
+    }
+public:
+
+    vector<vector<int>> findCriticalConections(int n, vector<vector<int>>& connections)
+    {
+        vector<int> nivel(n,0);
+        vector<int> nivel_min(n,0);
+        vector<int> viz(n,0);
+        vector<vector<int>> rez;
+        vector<vector<int>> connect;
+
+        for(int i=0;i<n;i++)
+            connect.push_back({});
+
+        for(int i=0;i<connections.size();i++) //rearranging the data
+        {
+            connect[connections[i][0]].push_back(connections[i][1]);
+            connect[connections[i][1]].push_back(connections[i][0]);
+        }
+
+        Dfs(0,connect,nivel,nivel_min,viz,rez);
+
+        return rez;
+    }
+
+    int dfs(int node, vector<vector<int>>& connect, vector<int>& safe) //returns 0 if the node is not safe, 1 if it is safe
+    {
+        if(safe[node]!=-1)
+            return safe[node];
+
+        safe[node] = 0;
+        for(int j=0;j<connect[node].size();j++)
+            if(dfs(connect[node][j],connect,safe)==0)
+                return safe[node];
+
+        safe[node] = 1;
+        return safe[node];
+    }
+
+    int BfsSearch(vector<vector<int>> grid) //searching by layers to find the 2nd island, helping function for shortest bridge
+    {
+
+        int x,y,n;
+        queue<vector<int>> coada;
+        n=grid.size();
+        st_land(x,y,grid);
+        filling(x,y,grid);
+        coada = bordering(grid);
+        return searching(coada,grid);
 
         return 0;
+    }
+
+    bool isBipartite(int n, vector<vector<int>>& connect)
+    {
+        queue<int> coada;
+        vector<int> culori (n+1,0);
+        int m=n;
+
+        coada.push(connect[1][0]);
+        culori[coada.front()]=1;
+
+        while(m>0)
+        {
+            int head = coada.front();
+            for(int i=0;i<connect[head].size();i++)
+            {
+                if(culori[connect[head][i]] == 0)
+                {
+                    coada.push(connect[head][i]);
+                    culori[connect[head][i]] = 3 - culori[head];
+                }
+                else if(culori[head] == culori[connect[head][i]])
+                    return false;
+
+            }
+            coada.pop();
+            m--;
+
+            if(coada.empty() && m!=0)
+                for(int j=1;j<=n;j++)
+                    if(culori[j]==0)
+                    {
+                       coada.push(j);
+                       break;
+                    }
+
+        }
+        return true;
+    }
+
+    vector<int> whichBipartite(int n, vector<vector<int>>& connect)
+    {
+        queue<int> coada;
+        vector<int> culori (n+1,0);
+        int m=n;
+
+        coada.push(connect[1][0]);
+        culori[coada.front()]=1;
+
+        while(m>0)
+        {
+            int head = coada.front();
+            for(int i=0;i<connect[head].size();i++)
+            {
+                if(culori[connect[head][i]] == 0)
+                {
+                    coada.push(connect[head][i]);
+                    culori[connect[head][i]] = 3 - culori[head];
+                }
+
+            }
+            coada.pop();
+            m--;
+
+            if(coada.empty() && m!=0)
+                for(int j=1;j<=n;j++)
+                    if(culori[j]==0)
+                    {
+                       coada.push(j);
+                       break;
+                    }
+
+        }
+        return culori;
+    }
+
+    int doFind(int x, vector<int> &parent)
+    {
+        while(parent[x]!=x)
+            x = doFind(parent[x],parent);
+
+        return x;
+    }
+
+    vector<int> doUnion(int a, int b,vector<int>& parent)
+    {
+        int ua = doFind(a,parent);
+        int ub = doFind(b,parent);
+
+        parent[ua] = ub;
+
+        return parent;
     }
 
     Graph()
@@ -201,76 +312,28 @@ public:
 
 class Solution
 {
-    int i,j;
-    vector<vector<int>> connect;
-    vector<int> nivel;
-    vector<int> nivel_min;
-    vector<int> viz;
-    vector<vector<int>> rez;
-    vector<int> safe;
-    vector<int> sol;
+
     queue<vector<int>> coada;
 
 public:
 
     bool possibleBipartition(int n, vector<vector<int>>& dislikes)
     {
+        int i;
+        Graph g;
+        vector<vector<int>> connect;
 
-        int colors[n+1],i,m;
-        m=n;
-        queue<int> coada;
-        for(i=1;i<=n;i++)
-            colors[i]=0; // in colors, 0 = unvisited, 1 = 1st color, 2= 2nd color
+        for(i=0;i<=n;i++)
+            connect.push_back({});
+        for(i=0;i<dislikes.size();i++)
+        {
+            connect[dislikes[i][0]].push_back(dislikes[i][1]);
+            connect[dislikes[i][1]].push_back(dislikes[i][0]);
+        }
 
         if(n>1)
-        {
-            coada.push(dislikes[0][0]);
-            colors[coada.front()]=1; //starting the queue
+            return g.isBipartite(n,connect);
 
-            while(m>0) //m = number of unvisited nodes
-            {
-                for(i=0;i<dislikes.size();i++)
-                {
-                    if(coada.front() == dislikes[i][0])
-                   {
-                        if(colors[dislikes[i][1]]==0) //if the pair has one unvisited member
-                        {
-                            coada.push(dislikes[i][1]);
-                            if(colors[coada.front()]==1) colors[dislikes[i][1]]=2;
-                                else colors[dislikes[i][1]]=1;
-                            //colored in the opposite color
-                        }
-                        else if(colors[dislikes[i][1]]==colors[coada.front()]) //checked if we colored the pair in 2 different colors
-                            return false;
-                   }
-
-                    //like above
-                    if(coada.front() == dislikes[i][1])
-                   {
-                        if(colors[dislikes[i][0]]==0)
-                        {
-                            coada.push(dislikes[i][0]);
-                            if(colors[coada.front()]==1) colors[dislikes[i][0]]=2;
-                                else colors[dislikes[i][0]]=1;
-                        }
-
-                        else if(colors[dislikes[i][0]]==colors[coada.front()])
-                            return false;
-                   }
-                }
-                coada.pop();
-                m--;
-
-                if(coada.empty() && m!=0)
-                    for(i=1;i<=n;i++)
-                        if(colors[i]==0)
-                        {
-                           coada.push(i);
-                           break;
-                        }
-                //if the queue is empty and we have unvisited nodes (m!=0), we push in queue the first unvisited node
-            }
-        }
         return true;
     }
 
@@ -327,37 +390,22 @@ public:
     vector<vector<int>> criticalConnections(int n, vector<vector<int>>& connections)
     {
         Graph g;
-        connect.clear();
-        for(i=0;i<n;i++) //initialing
-        {
-            nivel.push_back(0);
-            nivel_min.push_back(0);
-            viz.push_back(0);
-            connect.push_back({});
-        }
-
-        for(i=0;i<connections.size();i++) //rearranging the data
-        {
-            connect[connections[i][0]].push_back(connections[i][1]);
-            connect[connections[i][1]].push_back(connections[i][0]);
-        }
-
-        g.Dfs(0);
-
-        return rez;
+        return g.findCriticalConections(n,connections);
 
     }
 
     vector<int> eventualSafeNodes(vector<vector<int>>& graph)
     {
             Graph g;
-            connect = graph;
+            vector<vector<int>> connect;
+            vector<int> safe,sol;
 
+            connect = graph;
             for(int i=0;i<graph.size();i++)
                 safe.push_back(-1);
 
             for(int i=0;i<graph.size();i++)
-                if(g.dfs(i)==1)
+                if(g.dfs(i,connect,safe)==1)
                     sol.push_back(i);
 
             return sol;
@@ -371,14 +419,148 @@ public:
         return 0;
     }
 
+    bool equationsPossible(vector<string>& equations)
+    {
+        Graph g;
+        vector<int> parent(26);
+        int a,b;
+
+        for(int i=0;i<26;i++)
+            parent[i]=i;
+
+        for(int i=0;i<equations.size();i++)
+            if (equations[i][1]== '=')
+            {
+                a = equations[i][0] - 'a';
+                b = equations[i][3] - 'a';
+                parent = g.doUnion(a,b,parent);
+            }
+
+        for(int i=0;i<equations.size();i++)
+            if (equations[i][1]== '!')
+            {
+                a = equations[i][0] - 'a';
+                b = equations[i][3] - 'a';
+
+                if(g.doFind(a,parent) == g.doFind(b,parent))
+                    return false;
+            }
+        return true;
+
+    }
+
+    string noLongPaths(int n, vector<vector<int>>& connections)
+    {
+        Graph g;
+        vector<vector<int>> connect;
+        vector<int> colors(n+1);
+        string result = "";
+
+        for(int i=0;i<=n;i++)
+            connect.push_back({});
+        for(int i=0;i<connections.size();i++)
+        {
+            connect[connections[i][0]].push_back(connections[i][1]);
+            connect[connections[i][1]].push_back(connections[i][0]);
+        }
+
+        if(g.isBipartite(n,connect) == false)
+            return "NO";
+        else
+        {
+            colors = g.whichBipartite(n,connect);
+            for(int i=0;i<connections.size();i++)
+                if(colors[connections[i][0]]==1 && colors[connections[i][1]]==2)
+                    result += "1";
+                else
+                    result += "0";
+
+            return("YES\n" + result);
+        }
+    }
+
+    int minMaxDist(int n, vector<int>& marked, vector<vector<int>>& graph)
+    {
+
+        if(marked.size()==1)
+            return 0;
+
+        int distances[n + 1];
+        memset(distances, 0, sizeof(distances));
+        vector<vector<int>> connect(n + 1);
+        queue<int> coada;
+
+
+
+
+        for(int i=0;i<graph.size();i++)
+        {
+            connect[graph[i][0]].push_back(graph[i][1]);
+            connect[graph[i][1]].push_back(graph[i][0]);
+        }
+
+        coada.push(marked[0]);
+        while(!coada.empty())
+        {
+            for(int i=0;i<connect[coada.front()].size();i++)
+            {
+                int neighbour = connect[coada.front()][i];
+                if(distances[neighbour]==0 && neighbour!=marked[0])
+                {
+                    coada.push(neighbour);
+                    distances[neighbour] = distances[coada.front()] + 1;
+                }
+
+            }
+            coada.pop();
+        }
+
+
+
+        int max_distance = 0;
+        int st_extreme = -1;
+        for(int i=0;i<marked.size();i++)
+            if(distances[marked[i]]>max_distance)
+            {
+                max_distance = distances[marked[i]];
+                st_extreme = marked[i];
+            }
+
+
+        memset(distances, 0, sizeof(distances));
+
+
+        coada.push(st_extreme);
+        while(!coada.empty())
+        {
+            for(int i=0;i<connect[coada.front()].size();i++)
+            {
+                int neighbour = connect[coada.front()][i];
+                if(distances[neighbour]==0 && neighbour!=st_extreme)
+                {
+                    coada.push(neighbour);
+                    distances[neighbour] = distances[coada.front()] + 1;
+                }
+
+            }
+            coada.pop();
+        }
+
+        max_distance = 0;
+        for(int i=0;i<marked.size();i++)
+            if(distances[marked[i]]>max_distance)
+                max_distance = distances[marked[i]];
+
+
+        return ((max_distance+1)/2);
+
+    }
+
 };
 
 int main()
 {
 
-    Solution s;
-    vector<vector<int>> grid = {{1,1,1,1,1},{1,0,0,0,1},{1,0,0,0,1},{1,0,0,0,1},{0,0,1,0,0}};
-    cout<<s.shortestBridge(grid);
 
     return 0;
 }
